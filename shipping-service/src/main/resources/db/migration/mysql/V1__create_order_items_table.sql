c889d195bf95d23776c19df9a968cd07d4ec565c
Barcino44 <juanjobargracia@gmail.com>
2025-11-25 18:23:03 +0000

    Fix: Another migration error

--- shipping-service/src/main/resources/db/migration/mysql/V1__create_order_items_table.sql
+++ shipping-service/src/main/resources/db/migration/mysql/V1__create_order_items_table.sql
@@ -1,11 +1,8 @@
 CREATE TABLE order_items (
-    product_id INT NOT NULL,
-    order_id INT NOT NULL,
-    ordered_quantity INT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY (product_id, order_id),
-    FOREIGN KEY (product_id) REFERENCES products(product_id) ON DELETE CASCADE,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id) ON DELETE CASCADE
-);
-
+    product_id INT(11) NOT NULL,
+    order_id INT(11) NOT NULL,
+    ordered_quantity INT(11),
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (product_id, order_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
