a3e73a985b6fc5bd8871a48df68f0b8dfff0bd57
Barcino44 <juanjobargracia@gmail.com>
2025-11-25 16:51:23 +0000

    Fix: Migration flyway

--- favourite-service/src/main/resources/db/migration/h2/V1__create_favourites_table.sql
+++ favourite-service/src/main/resources/db/migration/h2/V1__create_favourites_table.sql
@@ -1,10 +1,10 @@
-
 CREATE TABLE favourites (
-	user_id INT(11) NOT NULL,
-	product_id INT(11) NOT NULL,
-	like_date TIMESTAMP DEFAULT LOCALTIMESTAMP NOT NULL NULL_TO_DEFAULT,
-	created_at TIMESTAMP DEFAULT LOCALTIMESTAMP NOT NULL NULL_TO_DEFAULT,
-	updated_at TIMESTAMP,
-	PRIMARY KEY (user_id, product_id, like_date)
+    user_id INT NOT NULL,
+    product_id INT NOT NULL,
+    like_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (user_id, product_id),
+    CONSTRAINT fk_fav_user FOREIGN KEY (user_id) REFERENCES users(user_id),
+    CONSTRAINT fk_fav_product FOREIGN KEY (product_id) REFERENCES products(product_id)
 );
-
--- shipping-service/src/main/resources/db/migration/mysql/V1__create_order_items_table.sql
+++ shipping-service/src/main/resources/db/migration/mysql/V1__create_order_items_table.sql
@@ -1,10 +1,12 @@
 
 CREATE TABLE order_items (
-	product_id INT(11) NOT NULL,
-	order_id INT(11) NOT NULL,
-	ordered_quantity INT(11),
-	created_at TIMESTAMP DEFAULT LOCALTIMESTAMP NOT NULL NULL_TO_DEFAULT,
-	updated_at TIMESTAMP,
-	PRIMARY KEY (product_id, order_id)
+    product_id INT NOT NULL,
+    order_id INT NOT NULL,
+    ordered_quantity INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (product_id, order_id),
+    FOREIGN KEY (product_id) REFERENCES products(product_id) ON DELETE CASCADE,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id) ON DELETE CASCADE
 );
 
